c929cd859cbddd34f468d3850c7bc653ee81d00d
Didem Gülşen Damlapınar <35114789+ddamlapinar@users.noreply.github.com>
2021-05-07 07:20:20 +0300

    add latest news section

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    navText: ["<img class='nav-back-icon'  src='img/back-icon.png'>","<img class='nav-next-icon' src='img/next-icon.png'>"],
+
+    responsive:{
+        0:{
+            items:1
+        }
+       
+    }
+})
